85325d0bd8d9baa77f3e3c8037c3a5273e30a15f
Oskar Dudycz <o.dudycz@lgbs.pl>
2017-02-13 23:20:14 +0100

    Fixes to netstndard

--- src/Shared.Core/IOC/IOCContainer.cs
+++ src/Shared.Core/IOC/IOCContainer.cs
@@ -1,19 +1,24 @@
-﻿using Ninject;
+﻿#if net45
+using Ninject;
+#endif
 using System.Collections.Generic;
 
 namespace GoldenEye.Shared.Core.IOC
 {
     public static class IOCContainer
     {
+#if NET45 && NET451 && NET452
         private static IKernel _kernel;
 
         public static void Initialize(IKernel kernel)
         {
             _kernel = kernel;
         }
+#endif
 
         public static T Get<T>()
         {
+#if NET45 && NET451 && NET452
             try
             {
                 return _kernel.Get<T>();
@@ -22,10 +27,14 @@ namespace GoldenEye.Shared.Core.IOC
             {
                 return default(T);
             }
+#elif NETSTANDARD1_5
+            return default(T);
+#endif
         }
 
         public static IEnumerable<T> GetAll<T>()
         {
+#if NET_45
             try
             {
                 return _kernel.GetAll<T>();
@@ -34,6 +43,9 @@ namespace GoldenEye.Shared.Core.IOC
             {
                 return new List<T>();
             }
+#elif NETSTANDARD1_5
+            return new List<T>();
+#endif
         }
     }
 }
